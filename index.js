a80160fda9f02a615b40b1c9d70498b67d86fe9f
Kabya55 <kabya55@example.com>
2026-06-16 03:13:25 +0600

    Setup Express server and basic middlewares

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
